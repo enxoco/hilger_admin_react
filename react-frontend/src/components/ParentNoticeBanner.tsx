d9b88e6d7bb5fe89f4e48aebe503a3ab4749c95b
Mike Conrad <mike@enxo.co>
2022-05-31 15:36:54 -0400

    Updated wording on parent banner

--- react-frontend/src/components/ParentNoticeBanner.tsx
+++ react-frontend/src/components/ParentNoticeBanner.tsx
@@ -11,7 +11,7 @@ export const ParentNoticeBanner = (props: any) => {
         <Box bg="bg-accent" color="on-accent" position="relative" w="100%">
           <Container py={{ base: "4", md: "3.5" }}>
             <Stack direction={{ base: "column", md: "row" }} justify="center" spacing={{ base: "0.5", md: "1.5" }} pe={{ base: "4", sm: "0" }}>
-              <Text fontWeight="medium">It looks like you have an unpaid balance.  In order to view your students report cards you will need to ensure that tuition has been paid in full.</Text>
+              <Text fontWeight="medium">You currently have an outstanding balance on your 2021-2022 school year's tuition. You will not be able to see your student(s) report card(s) until your tuition is paid in full. If you think you have paid this in full, or if you have questions about your balance, please contact Eddy Hilger at <a href="tel:423-653-1333">423.653.1333</a>.</Text>
             </Stack>
           </Container>
         </Box>
